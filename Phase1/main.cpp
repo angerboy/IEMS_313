72f9e01cdfd0920bf461494461ecdb8614cadae3
angerboy <matthewehinger2016@u.northwestern.edu>
2015-02-05 21:07:37 -0600

    Initial Commit

---
+++ Phase1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Phase1
+//
+//  Created by Matthew Ehinger on 2/5/15.
+//  Copyright (c) 2015 Matthew Ehinger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
